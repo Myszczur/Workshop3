3564762340724b44307e563d55f77c6d7c217ca6
Kamil <Kamil.Urbanik@o2.pl>
2022-05-27 14:28:50 +0200

    UserDelete v1.0

---
+++ jee-user-crud/src/main/java/pl/coderslab/servlet/UserDelete.java
@@ -0,0 +1,42 @@
+package pl.coderslab.servlet;
+
+import pl.coderslab.users.User;
+import pl.coderslab.users.UserDao;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet("/user/delete")
+public class UserDelete extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        UserDao userDao = new UserDao();
+
+        String idToDelete = request.getParameter("id");
+
+            int deleteId = Integer.parseInt(idToDelete);
+            User read = userDao.read(deleteId);
+            request.setAttribute("user", read);
+
+        getServletContext().getRequestDispatcher("/users/deleteUser.jsp")
+                .forward(request, response);
+        response.sendRedirect(request.getContextPath() + "/list");
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        UserDao userDao = new UserDao();
+
+        userDao.delete(Integer.parseInt(request.getParameter("id")));
+
+        response.sendRedirect(request.getContextPath() + "/list");
+
+
+    }
+}
